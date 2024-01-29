cf52c3ec4b93e7e3711046dcc66d1bebbb8d2b8c
Sebastian Varela <sebastianvarelavillegas@hotmail.com>
2024-01-29 11:22:35 -0500

    Creacion de todos los TestSeeders
    Se crearon  los TestSeeders de todos los modelos

---
+++ A3/database/seeders/TestCareerSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TestCareerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        
+    }
+}
